87d9db221885c548ca12379914cfb31304f08c8a
Aldian Varonitti <aldianvaro19@gmail.com>
2021-09-29 16:07:29 +0700

    Aldian Varonitti menambahkan Model_latihan1

---
+++ application/models/Model_latihan1.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Model_latihan1 extends CI_Model
+{
+    //membuat variable untuk menampung nilai
+    public $nilai1, $nilai2, $hasil;
+    //method penjualan
+    public function jumlah($n1 = null, $n2 = null)
+    {
+        $this->nilai1 = $n1;
+        $this->nilai2 = $n2;
+        $this->hasil = $this->nilai1 + $this->nilai2;
+        return $this->hasil;
+
+    }
+}
