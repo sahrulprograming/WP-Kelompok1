56d7d7a364891586ea3f72a6d40fb3fd6a9b76cb
sahrul <sahrulprograming@gmail.com>
2021-09-29 14:35:03 +0700

    sahrul melakukan konfigurasi CI3

---
+++ application/views/profile.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
+    <title>Profile</title>
+</head>
+
+<body>
+
+    <h1>Profile</h1>
+    <a href="<?= base_url() . "profile/about"; ?>" type="button" class="btn btn-primary">Primary</a>
+</body>
+
+<script src="<?= base_url('assets/js/bootstrap.bundle.min.js'); ?>"></script>
+
+</html>
