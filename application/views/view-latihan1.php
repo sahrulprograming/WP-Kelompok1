78110fa9ca610da5917691ce06cc1964ee4a2b14
sahrul <sahrulprograming@gmail.com>
2021-09-29 16:20:34 +0700

    Syahrul Menambahkan view-latihan1

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 1 Kelompok 1 | WP II</title>
+</head>
+
+<body>
+    <p>Halo Kawan.. Yuk kita belajar web programing..!!!!</p>
+    <p><label for="" style="display:inline-block;">Nilai 1</label> = <?= $nilai1; ?></p>
+    <p><label for="" style="display:inline-block;">Nilai 2</label> = <?= $nilai2; ?></p>
+    <p>ini hasil pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?></p>
+</body>
+
+</html>
