4575ab7224419e49d780231661fca2961a3d7053
MochamadNaufalSetiawan <mochamadnaufal03@gmail.com>
2021-09-29 16:56:01 +0700

    naufal menambahkan controller matkul

--- application/controllers/Matkul.php
+++ application/controllers/Matkul.php
@@ -5,9 +5,15 @@ class Matkul extends CI_Controller
 {
 	public function form()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('form_matkul');
 	}
 	public function tampil_data()
 	{
+		$data = [
+			'kode' => $this->input->post('kode'),
+			'nama' => $this->input->post('nama'),
+			'sks' => $this->input->post('sks')
+		];
+		$this->load->view('data_matkul', $data);
 	}
 }
