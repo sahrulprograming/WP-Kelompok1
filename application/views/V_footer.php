099c6c3227012e915877898f6607f51726297274
MochamadNaufalSetiawan <mochamadnaufal03@gmail.com>
2021-10-23 16:05:20 +0700

    Naufal Membuat Template Web Sederhana

---
+++ application/views/V_footer.php
@@ -0,0 +1,7 @@
+        <footer>
+            <a href="http://www.RentalBuku.com">rentalBuku</a>
+        </footer>
+        </div>
+        </body>
+
+        </html>
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Imam</li>
+        <li>Nama Belakang : Nawawi</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> jalan Ciledug Raya No. 168 Pesanggrahan</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Cirebon</li>
+    </ul>
+
+    <h4>Olah Raga Favotit</h4>
+    <ul type="square">
+        <li>bulutangkis</li>
+        <li>catur</li>
+    </ul>
+</section>
